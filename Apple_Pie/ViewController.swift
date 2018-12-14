a8982872a32ec01c3bc92720e892b285691febb5
bartwitting <bartwitting@icloud.com>
2018-12-14 12:47:39 +0100

    Comments

--- Apple_Pie/ViewController.swift
+++ Apple_Pie/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         }
     }
     
-    /// Building the app
+    /// Building the screen
     override func viewDidLoad() {
         super.viewDidLoad()
         newRound()
